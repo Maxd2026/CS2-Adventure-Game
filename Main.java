4a0d0ff3f492edd58cd298501585b70ad675bee2
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-12-02 16:06:25 +0000

    2

--- Main.java
+++ Main.java
@@ -8,19 +8,56 @@
         
        
       //STEP #1: Describe the current scenario
-      System.out.println("You are at the Colombus Circle entrance to Central Park.");
+      System.out.println("You are at an airport on a mysterious island");
       // STEP #2: Get user command
-      command = getCommand("Do you go east (e) or north (n)");
+      command = getCommand("Do you go east and get in the car waiting for you (c) or stay in the airport and leave the island (n)");
       // STEP #3: Select a path based on the user's command
-      if ( command.equals("e") ) {
+      if ( command.equals("c") ) {
          // NEXT BRANCH...
-         System.out.println("You reach the Carousel.");
+         System.out.println("You get in the car and drive to a mansion");
+
+         System.out.println("You arrive at mansion.");
+         command = getCommand("You can enter the mansion (e) or walk around and explore (w)");
+
+         if ( command.equals ("e") ) {
+
+            System.out.println("You enter the mansion and explore." );
+
+            command = getCommand("Open the creepy door (o) or go upstairs (u)");
+
+            if ( command.equals ("o") ) {
+               System.out.println("You see former president Bill Clinton and Steve Cohen eating lunch");
+            }
+            else if ( command.equals ("u") ) {
+               System.out.println("Donald Trump is waiting for you at the top of the stairs with a creepy grin.");
+            }
+
+         }
+         else if ( command.equals ("w") ) {
+            System.out.println("You see a man fishing by the water");
+
+            command = getCommand("Fish with the man (f)");
+
+            if ( command.equals ("f") ) {
+               System.out.println("You fish with the man and he warns you about the dangers of the island ");
+            }
+            else  {
+               System.out.println("The man pushes you into the water. GAME OVER. Press RUN to replay");
+            }
+            
+         }
+
+         // NEXT BRANCH
+        
+
+
+
       }
       else if ( command.equals("n") ) {
-         System.out.println("You see a wedding at Tavern on the Green");
+         System.out.println("You get on a plane back to NY");
       }
       else {
-         System.out.println("GAME OVER! You got lost in the Time Warner center shopping for hours. Press RUN to replay.");
+         System.out.println("GAME OVER! your plane crashes. Press RUN to replay.");
       }
 
 
@@ -35,7 +72,6 @@
        System.out.println("\n▶︎▶︎▶︎ " + prompt);
        Scanner scan = new Scanner(System.in);
        String command = scan.nextLine().toLowerCase();
-       scan.close();
        return command;
     } // END OF METHOD
  } // END OF CLASS
